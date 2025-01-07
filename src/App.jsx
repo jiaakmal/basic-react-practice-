27af713b4d8b08bb3e008e1bc0067d5431c11ff7
jiaakmal <160829841+jiaakmal@users.noreply.github.com>
2025-01-07 16:48:29 +0500

    injecting dynamic function component in header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-
+const reactDescription = ['Core' , 'Fundamental' , 'Curtail']
+function randomInt(max){
+  return Math.floor(Math.random() * (max+1));
+}
 export function Header(){
+  const description = reactDescription[randomInt(2)]
   return (
     <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
