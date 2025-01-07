febc7efae92e0718541cf12de4758a8684b660de
jiaakmal <160829841+jiaakmal@users.noreply.github.com>
2025-01-07 17:17:08 +0500

    Initializing props with "props" perimeter

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import reactImg from './assets/react-core-concepts.png'
+import componentsImg from './assets/components.png'
+
 const reactDescription = ['Core' , 'Fundamental' , 'Curtail']
 function randomInt(max){
   return Math.floor(Math.random() * (max+1));
@@ -6,7 +9,7 @@ export function Header(){
   const description = reactDescription[randomInt(2)]
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} />
         <h1>React Essentials</h1>
         <p>
           {description} React concepts you will need for almost any app you are
@@ -15,11 +18,39 @@ export function Header(){
       </header>
     );
 }
+// React call these functions and pass value to this props/argument under the hood . We only use 
+// these functions just like html elements ... And value used as props are in the form of objects 
+// key value pair..
+function CoreConcept(props) {
+  return (
+   <li>
+    <img src={props.image} alt={props.title} />
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+   </li>
+  );
+
+  
+}
+
 function App() {
   return (
     <div>
       <Header/>
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+            title="Components" 
+            description="The core UI building blocks"
+            image={componentsImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
