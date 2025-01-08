28e1cdbd94c4d50cc1fd72d6bcdc761ca48618da
jiaakmal <160829841+jiaakmal@users.noreply.github.com>
2025-01-08 15:56:47 +0500

    use state hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@ import { Header } from "./components/Header";
 import { CORE_CONCEPTS } from "./data";
 import { CoreConcept } from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
+import { useState } from "react";
+import { EXAMPLES } from "./data";
 
 
 
 
 function App() {
+
+const [selectedTopic, setSelectedTopic] = useState('components');
+
+
+function handleSelect(selectedButton) {
+setSelectedTopic(selectedButton);
+  console.log(selectedButton); 
+}
+
   return (
     <div>
       <Header />
@@ -27,11 +38,20 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={()=>handleSelect('Components')}>Components</TabButton>
+            <TabButton onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
